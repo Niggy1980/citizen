583917a3aaa53785e50b91b8e03903002c9f39c3
Niggy1980 <tawan.ph@ku.th>
2024-02-23 18:53:18 +0700

    finish login

--- lib/feature/USER/presentation/page/LOGINPAGE.dart
+++ lib/feature/USER/presentation/page/LOGINPAGE.dart
@@ -1,18 +1,18 @@
 import 'dart:js';
+import 'package:citizen/feature/USER/presentation/Homepage/HomePage.dart';
+import 'package:citizen/feature/USER/presentation/page/AdminPAGE.dart';
 import 'package:citizen/feature/USER/presentation/page/Register.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 
-final CollectionReference account = FirebaseFirestore.instance.collection('account');
-
-Future Information() async {
-  QuerySnapshot querySnapshot =
-      await FirebaseFirestore.instance.collection('account').get();
-}
+final CollectionReference account =
+    FirebaseFirestore.instance.collection('account');
+final Idcontroller = TextEditingController();
+final Passwordcontroller = TextEditingController();
 
 class LoginPage extends StatelessWidget {
-      LoginPage({super.key});
+  LoginPage({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -42,14 +42,21 @@ class LoginPage extends StatelessWidget {
                   //   ),]),
                   child: Align(
                     alignment: Alignment.center,
-                    child: Image(image: AssetImage('assets/image/LOGOCC.png'), width: 400,),
+                    child: Image(
+                      image: AssetImage('assets/image/LOGOCC.png'),
+                      width: 400,
+                    ),
                   ),
                 ),
                 Row(
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: [
                     Container(
-                      child: Icon(Icons.account_circle, color: Colors.blue, size: 40,),
+                      child: Icon(
+                        Icons.account_circle,
+                        color: Colors.blue,
+                        size: 40,
+                      ),
                     ),
                     SizedBox(
                       width: 10,
@@ -57,13 +64,16 @@ class LoginPage extends StatelessWidget {
                     Container(
                         // decoration: BoxDecoration(border: Border.all(color: Colors.green)),
                         child: Align(
-                        alignment: Alignment.center,
-                        child: SizedBox(
-                               width: 350,
-                               height: 50,
-                               child: TextField(
-                               decoration: InputDecoration(border: OutlineInputBorder(),labelText: "Enter your ID")),
-                        ),
+                      alignment: Alignment.center,
+                      child: SizedBox(
+                        width: 350,
+                        height: 50,
+                        child: TextField(
+                            controller: Idcontroller,
+                            decoration: InputDecoration(
+                                border: OutlineInputBorder(),
+                                labelText: "Enter your ID")),
+                      ),
                     )),
                   ],
                 ),
@@ -74,7 +84,11 @@ class LoginPage extends StatelessWidget {
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: [
                     Container(
-                      child: Icon(Icons.key, color: Colors.blue, size: 40,),
+                      child: Icon(
+                        Icons.key,
+                        color: Colors.blue,
+                        size: 40,
+                      ),
                     ),
                     SizedBox(
                       width: 10,
@@ -82,12 +96,17 @@ class LoginPage extends StatelessWidget {
                     Container(
                         // decoration: BoxDecoration(border: Border.all(color: Colors.blue)),
                         child: Align(
-                        alignment: Alignment.center,
-                        child: SizedBox(
+                      alignment: Alignment.center,
+                      child: SizedBox(
                         width: 350,
                         height: 50,
                         child: TextField(
-                            obscureText: true,decoration: InputDecoration(border: OutlineInputBorder(),labelText: "Enter your Password",)),
+                            controller: Passwordcontroller,
+                            obscureText: true,
+                            decoration: InputDecoration(
+                              border: OutlineInputBorder(),
+                              labelText: "Enter your Password",
+                            )),
                       ),
                     )),
                   ],
@@ -104,18 +123,72 @@ class LoginPage extends StatelessWidget {
                 // ), borderrrrrrrrrrr
                 Container(
                     child: Align(
-                    alignment: Alignment.center,
-                    child: ElevatedButton(
+                  alignment: Alignment.center,
+                  child: ElevatedButton(
                     child: Text(
-                      "Login",style: TextStyle(color: Colors.white),
+                      "Login",
+                      style: TextStyle(color: Colors.white),
                     ),
                     onPressed: () {
-                      Navigator.pushNamed(context, '/homepage');
+                      Future Information() async {
+                        QuerySnapshot querySnapshot = await FirebaseFirestore.instance.collection('account')
+                            .where('id', isEqualTo: Idcontroller.text).where('password', isEqualTo: Passwordcontroller.text).get();
+
+                        if (querySnapshot.docs.isNotEmpty) {
+                          for (QueryDocumentSnapshot documentSnapshot
+                              in querySnapshot.docs) {
+                            Map<String, dynamic> data = documentSnapshot.data() as Map<String, dynamic>;
+                            String ID = data['id'];
+                            String Password = data['password'];
+                            String Role = data['role'];
+                            print('id: $ID,password: $Password, role: $Role');
+                            if (Role == 'user') {
+                              Navigator.of(context).push(
+                                MaterialPageRoute(builder: (_) => HomePage()),
+                              );
+                            } else {
+                              Navigator.of(context).push(
+                                MaterialPageRoute(
+                                    builder: (_) => MainAdminPage()),
+                              );
+                            }
+                          }
+                        }
+                        if (querySnapshot.docs.isEmpty) {
+                          AlertDialog alert = AlertDialog(
+                            title: Text('Alert',
+                                style: TextStyle(
+                                  color: Colors.red,
+                                  fontWeight: FontWeight.bold,
+                                )),
+                            content: Text('Please check your ID or Password'),
+                            actions: [
+                              TextButton(
+                                child: Text('OK'),
+                                onPressed: () {
+                                  Navigator.of(context).pop();
+                                },
+                              ),
+                            ],
+                          );
+                          showDialog(
+                            context: context,
+                            builder: (BuildContext context) {
+                              return alert;
+                            },
+                          );
+                          return;
+                        }
+                      }
+                      Information();
+
 
                     },
-                    style: ElevatedButton.styleFrom(backgroundColor: Colors.blue,),),
-                    )
-                ),
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.blue,
+                    ),
+                  ),
+                )),
                 SizedBox(
                   height: 10,
                 ),
@@ -130,7 +203,8 @@ class LoginPage extends StatelessWidget {
                     Container(
                       child: TextButton(
                         onPressed: () {
-                          Navigator.of(context).push(MaterialPageRoute(builder: (_) => Register()),
+                          Navigator.of(context).push(
+                            MaterialPageRoute(builder: (_) => Register()),
                           );
                         },
                         child: Text("register"),
