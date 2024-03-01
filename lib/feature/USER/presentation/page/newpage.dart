f981a2ffb95b848ce272005c78253a0063a4c6dc
Garfield567 <86929560+Garfield567@users.noreply.github.com>
2024-03-01 22:12:59 +0700

    Update newpage.dart

---
+++ lib/feature/USER/presentation/page/newpage.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+
+ class Newspage extends StatelessWidget {
+   const Newspage({super.key});
+ 
+   @override
+   Widget build(BuildContext context) {
+     return  MaterialApp(
+       home: Scaffold(
+
+       ),
+
+     );
+
+   }
+ }
+ 
