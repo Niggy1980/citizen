110431abddcbfe355718d909a29f19e4c313e9bf
Garfield567 <86929560+Garfield567@users.noreply.github.com>
2024-03-02 22:50:05 +0700

    Update newpage.dart

--- lib/feature/USER/presentation/page/newpage.dart
+++ lib/feature/USER/presentation/page/newpage.dart
@@ -1,18 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_auth';
+
+class Newspage extends StatelessWidget {
+  const Newspage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return  MaterialApp(
+      home: Scaffold(
+
+      ),
+
+    );
+
+  }
+}
 
 
- class Newspage extends StatelessWidget {
-   const Newspage({super.key});
- 
-   @override
-   Widget build(BuildContext context) {
-     return  MaterialApp(
-       home: Scaffold(
 
-       ),
 
-     );
 
-   }
- }
- 
