a28f6764c93fc72134619a12ab04a17a828d80ca
poonnx <122014099+poonnx@users.noreply.github.com>
2024-03-03 15:33:33 +0700

    Create Comment.dart

---
+++ lib/feature/USER/Function/Comment.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+class Comment extends StatelessWidget {
+  final String text;
+  final String time;
+  const Comment({super.key,
+  required this.text,
+    required this.time,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        color: Colors.grey[300],
+        borderRadius: BorderRadius.circular(4),
+      ),
+      child: Column(
+        children: [
+          //comment
+          Text(text),
+
+          //user, time
+          Row(
+            children: [
+              Text("."),
+              Text(time),
+            ],
+          ),
+
+
+
+        ],
+      ),
+
+
+    );
+  }
+}
