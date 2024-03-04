82934cb57cd75446e8e11d6da66c5a9275b9a416
Garfield567 <86929560+Garfield567@users.noreply.github.com>
2024-03-04 20:54:15 +0700

    Update newpage.dart

--- lib/feature/USER/presentation/page/newpage.dart
+++ lib/feature/USER/presentation/page/newpage.dart
@@ -10,7 +10,8 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text('อุ'),
+          backgroundColor: Colors.pink,
+          title: Text('rest Api '),
         ),
         body:  Center(
           child: Image.asset("assets/หหห"
