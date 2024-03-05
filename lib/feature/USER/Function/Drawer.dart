494ff21c9e25e225face5df81c932394cf0c95aa
poonnx <122014099+poonnx@users.noreply.github.com>
2024-03-06 02:09:37 +0700

    update

--- lib/feature/USER/Function/Drawer.dart
+++ lib/feature/USER/Function/Drawer.dart
@@ -35,10 +35,19 @@ class MyDrawer extends StatelessWidget {
             title: Text("F Y Q"),textColor: Colors.white,
             onTap: (){
               Navigator.pop(context);
-              Navigator.pushNamed(context, '/');
+              Navigator.pushNamed(context, '/faqpage');
             },
           ),
-          SizedBox(height:310),
+          ListTile(
+            leading: Icon(Icons.phone,color: Colors.white,),
+            title: Text("C O N T A C T"),textColor: Colors.white,
+            onTap: (){
+              Navigator.pop(context);
+              Navigator.pushNamed(context, '/contactpage');
+            },
+
+          ),
+          SizedBox(height:320),
           Padding(
             padding: const EdgeInsets.only(bottom: 25.0),
             child: ListTile(
